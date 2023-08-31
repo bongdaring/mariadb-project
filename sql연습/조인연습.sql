4d416f3ab63e7895a2797978a78f84f3b2cfd216
bongdaring <chlwlsdud258@gmail.com>
2023-08-31 12:10:28 +0900

    조인 #3

--- sql연습/조인연습.sql
+++ sql연습/조인연습.sql
@@ -69,3 +69,53 @@ select count(*)
 	from titles a join salaries b on a.emp_no = b.emp_no
 where a.to_date = '9999-01-01'
 	and b.to_date = '9999-01-01';
+    
+    
+-- 실습문제1
+-- 현재, 직원별 근무 부서를 출력해보세요.
+-- 사번, 직원이름(first_name), 부서명 순으로 출력
+select e.emp_no, e.first_name, d.dept_name
+	from employees e, departments d, dept_emp de
+where e.emp_no = de.emp_no
+	and d.dept_no = de.dept_no
+    and de.to_date = '9999-01-01';
+
+-- 실습문제2
+-- 현재, 직책별 평균 연봉과 직원 수를 구하되 직책별 직원 수가 100명이상인 직책만 출력하세요.
+-- 직책, 평균 연봉, 직원 수 순으로 출력
+select title, avg(salary), count(*)
+	from titles t, salaries s
+where t.emp_no = s.emp_no
+	and t.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+group by t.title
+	having count(*) >= 100;
+    
+-- 실습문제3
+-- 현재, 부서별로 직책이 Engineer인 지원들에 대해서만 평균연봉을 구하세요.
+-- 부서이름, 평균급여로 출력하고 평균 연봉이 높은 순으로 정렬하세요.
+select b.emp_no, t.title, d.dept_name, avg(s.salary)
+	from departments d, dept_emp b, titles t, salaries s
+where d.dept_no = b.dept_no
+	and b.emp_no = t.emp_no
+    and t.emp_no = s.emp_no
+    and b.to_date = '9999-01-01'
+    and t.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+    and t.title = 'Engineer'
+group by d.dept_name
+order by avg(s.salary) desc;
+
+select d.dept_name, avg(s.salary)
+	from departments d 
+    join dept_emp de on d.dept_no = de.dept_no
+    join titles t on de.emp_no = t.emp_no
+	join salaries s on t.emp_no = s.emp_no
+where t.to_date = '9999-01-01'
+	and de.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+	and t.title like 'Engineer'
+group by d.dept_name
+order by avg(s.salary) desc;
+
+    
