a6a9b87dc948aa96cdebdb39590cc65e1d4dd086
bongdaring <chlwlsdud258@gmail.com>
2023-09-01 11:15:36 +0900

    transaction

--- sql연습/DDL.sql
+++ sql연습/DDL.sql
@@ -32,3 +32,56 @@ desc emeber;
 --
 -- dml
 --
+
+-- insert
+insert 
+	into member
+values (null, 'mysql@mysql.com', password('1234'), '안대혁', '개발팀', null);
+select * from member;
+
+insert 
+	into member(no, email, password, name, dept, self_intro)
+values (null, 'mysql@mysql.com', password('1234'), '마이2', '개발팀2', null);
+select * from member;
+
+insert 
+	into member(email, password, name, dept)
+values ('mysql@mysql.com', password('1234'), '마이3', '개발팀3');
+select * from member;
+
+insert 
+	into member(email, name, dept, password)
+values ('mysql@mysql.com', '마이4', '개발팀4', password('1234'));
+select * from member;
+
+
+-- update
+update member 
+	set email='mariadb@mariadb.com', name='마리아'
+where no = 4;
+select * from member;
+
+-- delete 
+delete 
+	from member 
+where no = 4;
+select * from member;
+
+
+-- transaction begin
+select @@autocommit from dual;
+
+-- transaction begin
+set autocommit = 0;
+select @@autocommit from dual;
+
+insert 
+	into member(email, name, dept, password)
+values ('mysql@mysql.com', '마이4', '개발팀4', password('1234'));
+select * from member;
+
+select no, email, dept from member;
+
+commit;
+
+select no, email, dept from member;
