77be8f80d6d1b4ed2966c998fe25c35f4f6ad621
bongdaring <chlwlsdud258@gmail.com>
2023-09-02 15:03:04 +0900

    practice #4

--- sql-practices/practice4.sql
+++ sql-practices/practice4.sql
@@ -9,7 +9,7 @@ where s.salary > (select avg(s.salary)
 
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서, 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
-select e.emp_no, e.first_name, max_salary.dept_name, max_salary.maxi
+select e.emp_no, CONCAT(e.first_name, ' ', e.last_name), max_salary.dept_name, max_salary.maxi
 	from employees e, (	select de.emp_no, d.dept_name, max(s.salary) as maxi
 							from departments d join dept_emp de on d.dept_no = de.dept_no
 							join salaries s on de.emp_no = s.emp_no
@@ -21,11 +21,11 @@ order by maxi desc;
 
 -- 문제3.
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
-select e.emp_no, e.first_name, s.salary
+select e.emp_no, CONCAT(e.first_name, ' ', e.last_name), s.salary
 	from employees e join dept_emp de on e.emp_no = de.emp_no
     join salaries s on e.emp_no = s.emp_no 
     join (	select de.dept_no, avg(s.salary) as avg_salary
-				from dept_emp de join salaries s on de.emp_no = s.salary
+				from dept_emp de join salaries s on de.emp_no = s.emp_no
 			where de.to_date = '9999-01-01'
 				and s.to_date = '9999-01-01'
 			group by de.dept_no) avg_de on de.dept_no = avg_de.dept_no
@@ -35,12 +35,16 @@ where s.salary > avg_de.avg_salary
 
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
-select e.emp_no as '사번', e.first_name as '이름', dep_manage.first_name as '매니저 이름', dep_manage.dept_name as '부서'
+select e.emp_no as '사번', 
+	   CONCAT(e.first_name, ' ', e.last_name) as '이름', 
+       CONCAT(dep_manage.first_name, ' ', dep_manage.last_name) as '매니저 이름', 
+       dep_manage.dept_name as '부서'
 	from employees e join dept_emp de on e.emp_no = de.emp_no
-    join (	select d.dept_no, d.dept_name, e.emp_no, e.first_name
+    join (	select d.dept_no, d.dept_name, e.emp_no, e.first_name, e.last_name
 				from dept_manager dm join departments d on dm.dept_no = d.dept_no
 				join employees e on e.emp_no = dm.emp_no
-			where dm.to_date = '9999-01-01') dep_manage on de.dept_no = dep_manage.dept_no;
+			where dm.to_date = '9999-01-01') dep_manage on de.dept_no = dep_manage.dept_no
+	where de.to_date = '9999-01-01';
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
@@ -48,11 +52,14 @@ select e.emp_no, e.first_name, t.title, s.salary
 	from employees e join dept_emp de on e.emp_no = de.emp_no
     join titles t on e.emp_no = t.emp_no
     join salaries s on e.emp_no = s.emp_no
-    join (	select de.dept_no
+    join (	select dept_no, AVG(salary) AS avg_salary
 				from dept_emp de join salaries s on de.emp_no = s.emp_no
+			where 
+				    de.emp_no = s.emp_no
+                    AND de.to_date = '9999-01-01'
+                    AND s.to_date = '9999-01-01'
 			group by de.dept_no
-				having avg(s.salary)
-			order by avg(s.salary) desc limit 1
+			order by avg_salary desc limit 0, 1
 ) max_dept on de.dept_no = max_dept.dept_no
 where de.to_date = '9999-01-01';
 
